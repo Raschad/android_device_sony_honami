800e61afc80e9c19be2ddac2b4095c9a4c0bc5f6
Raschad <Raschad@yandex.ru>
2018-12-27 01:17:27 +0300

    Update and rename pixys_honami.mk to du_honami.mk

--- du_honami.mk
+++ du_honami.mk
@@ -1,5 +1,5 @@
 # Inherit lineage common Phone stuff.
-$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 $(call inherit-product, device/sony/honami/full_honami.mk)
 
@@ -8,9 +8,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.1.1 14.6.A.1.23
 
 BUILD_FINGERPRINT := Sony/C6903/C6903:5.1.1/14.6.A.1.236/2031203603:user/release-keys
 
-TARGET_DENSITY := xxhdpi
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_BOOTANIMATION_HALF_RES := true
+#TARGET_DENSITY := xxhdpi
+#TARGET_BOOT_ANIMATION_RES := 1080
+#TARGET_BOOTANIMATION_HALF_RES := true
 
-PRODUCT_NAME := pixys_honami
+PRODUCT_NAME := du_honami
 PRODUCT_DEVICE := honami
